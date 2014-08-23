71bf308f6464def6931a913a33b664a0447891d0
Jacek Złydach <temporal.pl@gmail.com>
2014-08-23 05:04:51 +0200

    Pointed right executable to SLIME, + multiple-cursors mode.

--- emacs24/.emacs.conf/slime.el
+++ emacs24/.emacs.conf/slime.el
@@ -1,4 +1,4 @@
-(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
 (add-to-list 'load-path "/home/temporal/repos/slime")
 (try-load-and-configure-library
  (require 'slime)
