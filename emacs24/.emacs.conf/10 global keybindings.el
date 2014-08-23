244c1fae9e40d106105c8382152bc744dcfb53c0
Jacek Złydach <temporal.pl@gmail.com>
2014-08-24 01:51:05 +0200

    Paredit tweaks + frame resizing keys.

--- emacs24/.emacs.conf/10 global keybindings.el
+++ emacs24/.emacs.conf/10 global keybindings.el
@@ -9,3 +9,10 @@
 (global-set-key (kbd "M-j") (lambda ()
                               (interactive)
                               (join-line -1)))
+
+
+;;; Frame resizing
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
