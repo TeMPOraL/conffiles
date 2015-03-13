4f57d8ac36c8696f973403b1c8ae46ad52dccfc4
TeMPOraL <temporal.pl@gmail.com>
2015-03-13 03:21:21 +0100

    Re-enabled SBCL.

--- emacs24/.emacs.conf/slime.el
+++ emacs24/.emacs.conf/slime.el
@@ -1,4 +1,3 @@
-;; (setq inferior-lisp-program "/usr/local/bin/sbcl")
-;; (add-to-list 'load-path "/home/temporal/repos/slime")
 (try-load-and-configure-library 'slime
+                                (setq inferior-lisp-program "/usr/local/bin/sbcl")
                                 (slime-setup '(slime-fancy)))
